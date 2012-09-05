8fede7c671f96961291978a6cd26d556018b7fa4
kinaba <kiki@kmonos.net>
2012-09-05 21:55:05 +0900

    Add more configurations.

--- src/oracle.js
+++ src/oracle.js
@@ -24,11 +24,10 @@ var Oracle =
 {
 	"Angel": function(N, Output)
 	{
-		// fact : int[N][N]
-		//   fact[a][b] =  0 なら、aとbの大小は未確定
-		//   fact[a][b] = -1 なら、a < b が確定している
-		//   fact[a][b] = +1 なら、a > b が確定している
-		// ことを表す
+		// fact[a][b] = K_LESS means that we already know a<b.
+		// fact[a][b] = K_MORE means that we already kwno a>b.
+		// fact[a][b] = K_UNKNOWN means nothing is known.
+
 		var fact = new Array(N);
 		for(var i=0; i<N; ++i) {
 			fact[i] = new Array(N);
@@ -42,24 +41,24 @@ var Oracle =
 
 			if( fact[a][b] == K_UNKNOWN )
 			{
-				// a < b としたら何個確定が増えるか？
+				// How many cells can we fill if a<b ?
 				var ab = 0;
 				for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
 				for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
 					if(x!=y && fact[x][y]==K_UNKNOWN)
 						ab++;
 
-				// b < a としたら何個確定が増えるか？
+				// How many cells can we fill if b<a ?
 				var ba = 0;
 				for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
 				for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
 					if(x!=y && fact[x][y]==K_UNKNOWN)
 						ba++;
 
-				// Angel は確定が増える方を選ぶ
+				// Angel wants to make sort faster, so she likes to fill more cells.
 				if( ab > ba )
 				{
-					// a < b としました。
+					// It's a<b!
 					for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
 					for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
 						if(x!=y && fact[x][y]==K_UNKNOWN) {
@@ -69,7 +68,7 @@ var Oracle =
 				}
 				else
 				{
-					// b < a としました。
+					// It's b<a!
 					for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
 					for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
 						if(x!=y && fact[x][y]==K_UNKNOWN) {
@@ -98,24 +97,20 @@ var Oracle =
 
 			if( fact[a][b] == K_UNKNOWN )
 			{
-				// a < b としたら何個確定が増えるか？
 				var ab = 0;
 				for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
 				for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
 					if(x!=y && fact[x][y]==K_UNKNOWN)
 						ab++;
 
-				// b < a としたら何個確定が増えるか？
 				var ba = 0;
 				for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
 				for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
 					if(x!=y && fact[x][y]==K_UNKNOWN)
 						ba++;
 
-				// Devil は確定が減る方を選ぶ
-				if( ab < ba )
+				if( ab < ba ) // Only difference between a devil and an angel.
 				{
-					// a < b としました。
 					for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
 					for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
 						if(x!=y && fact[x][y]==K_UNKNOWN) {
@@ -125,7 +120,58 @@ var Oracle =
 				}
 				else
 				{
-					// b < a としました。
+					for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
+					for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
+						if(x!=y && fact[x][y]==K_UNKNOWN) {
+							fact[x][y] = K_LESS;
+							fact[y][x] = K_MORE;
+						}
+				}
+			}
+			Output.push(fact[a][b]==K_LESS ? [a,b] : [b,a]);
+			return (fact[a][b] == K_LESS);
+		};
+	},
+
+	"Random": function(N, Output)
+	{
+		var fact = new Array(N);
+		for(var i=0; i<N; ++i) {
+			fact[i] = new Array(N);
+			for(var j=0; j!=N; ++j)
+				fact[i][j] = K_UNKNOWN;
+		}
+
+		return function(a, b) {
+			if( a == b )
+				return false;
+
+			if( fact[a][b] == K_UNKNOWN )
+			{
+				var ab = 0;
+				for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
+				for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
+					if(x!=y && fact[x][y]==K_UNKNOWN)
+						ab++;
+
+				var ba = 0;
+				for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
+				for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
+					if(x!=y && fact[x][y]==K_UNKNOWN)
+						ba++;
+
+				// Biased randomness. If ab is small, a<b is likely to happen than b<a.
+				if( Math.random() < Math.sqrt(ba)/(Math.sqrt(ab)+Math.sqrt(ba)) )
+				{
+					for(var x=0; x<N; ++x) if(x==a || fact[x][a]==K_LESS)
+					for(var y=0; y<N; ++y) if(y==b || fact[b][y]==K_LESS)
+						if(x!=y && fact[x][y]==K_UNKNOWN) {
+							fact[x][y] = K_LESS;
+							fact[y][x] = K_MORE;
+						}
+				}
+				else
+				{
 					for(var x=0; x<N; ++x) if(x==b || fact[x][b]==K_LESS)
 					for(var y=0; y<N; ++y) if(y==a || fact[a][y]==K_LESS)
 						if(x!=y && fact[x][y]==K_UNKNOWN) {
